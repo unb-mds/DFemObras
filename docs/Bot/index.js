ca6f455bfb1c7b694a330f7f3d44f251200ccce7
Nathan BS <143636158+Nathan-bs@users.noreply.github.com>
2024-11-26 17:31:27 -0300

    Update index.js

---
+++ docs/Bot/index.js
@@ -0,0 +1,24 @@
+// Importando a biblioteca twitter-api-v2
+const { TwitterApi } = require('twitter-api-v2');
+
+// Credenciais da API do Twitter (substitua pelos seus dados)
+const client = new TwitterApi({
+  appKey: '',
+  appSecret: '',
+  accessToken: '',
+  accessSecret: '',
+});
+
+// Função para enviar um tweet
+async function sendTweet() {
+  try {
+    // Envia um tweet
+    const tweet = await client.v2.tweet('Olá, mundo! Este é um tweet automatizado!');
+    console.log('Tweet enviado com sucesso:', tweet.data.text);
+  } catch (error) {
+    console.error('Erro ao enviar o tweet:', error);
+  }
+}
+
+// Chama a função para enviar o tweet
+sendTweet();
