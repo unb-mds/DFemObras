409df92814064fa0575268a01353191818473093
Nathan-bs <lunathanbat@gmail.com>
2025-01-14 17:00:36 -0300

    Código agora com funções para apuração de testes (Refatoração de código)

--- Bots/bot_Twitter.py
+++ Bots/bot_Twitter.py
@@ -4,6 +4,7 @@ import tweepy
 from dotenv import load_dotenv
 
 load_dotenv()
+
 API_COHERE_KEY = os.getenv("API_COHERE_KEY")
 API_CONSUMER_KEY_X = os.getenv("API_CONSUMER_KEY_X")
 API_CONSUMER_SECRET_X = os.getenv("API_CONSUMER_SECRET_X")
@@ -19,18 +20,40 @@ api = tweepy.Client(
     access_token_secret=API_ACCESS_TOKEN_SECRET_X
 )
 
-try:
-    message = "Explique em 4 linhas, até 50 caracteres por linha e em tópicos de onde vêm as verbas para obras públicas."
-    response = co.chat(
-        model="command-r-plus", 
-        messages=[{"role": "user", "content": message}]
-    )
+def generate_message(co_client, message_prompt):
+    try:
+        response = co_client.chat(
+            model="command-r-plus",
+            messages=[{"role": "user", "content": message_prompt}]
+        )
+        return response.message.content[0].text
+    except Exception as e:
+        print("Erro ao gerar mensagem:", e)
+        return None
 
-    mensagem_principal = response.message.content[0].text
-    print(mensagem_principal)
 
-    tweet = api.create_tweet(text=mensagem_principal)
-    print("Tweet enviado com sucesso:", tweet) 
-    
-except Exception as e:
-    print("Algo deu erro:", e)
+def post_tweet(api_client, message):
+    try:
+        tweet = api_client.create_tweet(text=message)
+        print("Tweet enviado com sucesso:", tweet)
+        return tweet
+    except Exception as e:
+        print("Erro ao enviar o tweet:", e)
+        return None
+
+def main():
+    try:
+        message_prompt = ("Explique em 4 linhas, até 50 caracteres por linha e em tópicos como estão sendo organizadas as obras do DF")
+        
+        mensagem_principal = generate_message(co, message_prompt)
+        print("Mensagem gerada:", mensagem_principal)
+
+        response = post_tweet(api, mensagem_principal)
+        print("Resposta da API do Twitter:", response)
+
+    except Exception as e:
+        print("Ocorreu um erro:", e)
+
+
+if __name__ == "__main__":
+    main()
