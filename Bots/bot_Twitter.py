66442689c9451d84411d3064ee730166e39fd16b
Cayo Alencar <cayoalencar2345@gmail.com>
2025-02-10 16:47:17 -0300

    fix: corrigindo fonte das imagens geradas pelo bot

--- Bots/bot_Twitter.py
+++ Bots/bot_Twitter.py
@@ -79,11 +79,14 @@ def save_image(data, output_dir):
         width, height = 1800, 1200
 
         try:
-            font_path = "arial.ttf"  
-            font = ImageFont.truetype(font_path, size=20)  
+            font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf" 
+            if not os.path.exists(font_path):
+                font_path = "./assets/fonts/arial.ttf"  
+            font = ImageFont.truetype(font_path, size=20)
         except Exception as e:
             print(f"Erro ao carregar a fonte: {e}. Usando fonte padrão.")
-            font = ImageFont.load_default()  
+            font = ImageFont.load_default()
+  
 
         images_created = 0
         items_per_image = len(data) // 4 + (1 if len(data) % 4 != 0 else 0)
@@ -130,7 +133,8 @@ def save_image(data, output_dir):
                         break
 
                    
-                    draw.text((10, y_offset), line, fill="black", font=font)
+                    clean_line = line.replace("\u200b", "").encode("utf-8", "ignore").decode("utf-8")
+                    draw.text((10, y_offset), clean_line, fill="black", font=font)
                     y_offset += text_height + 10  
 
                 
