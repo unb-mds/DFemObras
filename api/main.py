ff1c4a80a536269f2db51327b78519dc91d1fc93
Caio Melo Borges <caioborges250802@gmail.com>
2026-02-21 21:16:48 -0300

    fix: fixing requirements

--- api/main.py
+++ api/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "https://unb-mds.github.io",
         "http://localhost:5500",      
-        "http://127.0.0.1:5500"       
+        "https://df-em-obras-frontend.vercel.app"       
     ],
     allow_methods=["*"],
     allow_headers=["*"],
